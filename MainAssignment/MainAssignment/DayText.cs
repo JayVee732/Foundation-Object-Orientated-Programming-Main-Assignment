2439891e1ef6485ddaef1bc23eb53c40f9fb2a7b
PsychoHacks <PsychoHacks@users.noreply.github.com>
2017-05-03 16:26:28 +0100

    +Working on some minor things, will all be changed through-out working on this project

---
+++ MainAssignment/MainAssignment/DayText.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainAssignment
+{
+    class DayText
+    {
+        public DateTime Year { get; set; }
+        public string Text { get; set; }
+        
+        public DayText ()
+        {
+
+        }
+
+        public DayText(DateTime year, string text)
+        {
+            this.Year = year;
+            this.Text = text;
+        }
+    }
+}
--- MainAssignment/MainAssignment/MainWindow.xaml.cs
+++ MainAssignment/MainAssignment/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +25,33 @@ namespace MainAssignment
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            calDisplay.FirstDayOfWeek = DayOfWeek.Monday;
+            calDisplay.IsTodayHighlighted = true;
+            calDisplay.SelectedDates.Add(new DateTime(2017, 3, 5));
+
+            DayText d1 = new DayText { Year = 2017, 5, 3, Text = "test1!"};
+
+
+
+            //using (StreamReader sr = new StreamReader("testDates.txt"))
+            //{
+            //    string day = sr.ReadLine();
+            //    while (day != null)
+            //    {
+            //        string[] newDay = day.Split(',');
+            //        string checkDay = newDay[0];                    
+            //    }
+            //    sr.Close();
+            //}
+        }
+
+        private void calDisplay_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SelectedDateTextBox.Text = calDisplay.SelectedDate.ToString();
+        }
     }
 }
+    
